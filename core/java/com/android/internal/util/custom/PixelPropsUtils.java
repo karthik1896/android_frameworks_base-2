PixelExperience/frameworks_base

PixelPropsUtils: Override fingerprint for SettingsIntelligence
Change-Id: Ia839345da18dde46bfd1acb77b42e0c47d28c8ac

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -87,6 +87,10 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
+        // Set proper indexing fingerprint
+        if (packageName.equals("com.google.android.settings.intelligence")){
+            setPropValue("FINGERPRINT", Build.DATE);
+        }
     }
 
     private static void setPropValue(String key, Object value){

karthik1896/android_frameworks_base-2

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -87,6 +87,10 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
+        // Set proper indexing fingerprint
+        if (packageName.equals("com.google.android.settings.intelligence")){
+            setPropValue("FINGERPRINT", Build.EXTENDED_FINGERPRINT);
+        }
     }
 
     private static void setPropValue(String key, Object value){